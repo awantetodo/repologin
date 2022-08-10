9e1fab737d92bc44bdeab7ef9b49fd4833281693
awantetodo <ignaciocasco@hotmail.com>
2022-08-10 01:14:07 -0300

    first commit

---
+++ src/config/dbConfig.js
@@ -0,0 +1,11 @@
+ const config = {
+    mongoDb: {
+        url: 'mongodb+srv://nachitoadmin:sarasa123@cluster0.0hapq3b.mongodb.net/desClass22?retryWrites=true&w=majority',
+        options: {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }
+    }
+}
+
+export default config;
